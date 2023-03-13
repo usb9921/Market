99d163726ac1c2b67ff9a824bb09fb5578657339
12usbaviskar97 <umeshbaviskar4515@gmail.com>
2023-03-13 18:42:11 +0530

    send to repo

---
+++ src/main/java/com/yctc/market/MarketProApplication.java
@@ -0,0 +1,13 @@
+package com.yctc.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yctc/market/MarketProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yctc.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
